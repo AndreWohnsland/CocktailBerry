9c4eb206157177d5c5e5f863ed31a3ec45634423
Andre Wohnsland <Andre_Wohnsland@web.de>
2019-05-27 20:33:16 +0200

    added Decorators

--- loggerconfig.py
+++ loggerconfig.py
@@ -1,15 +1,18 @@
 """ Special configs for the Logger """
 import logging
 import time
+from functools import wraps
+
+import globals
 
 
-def basiclogger(loggername):
+def basiclogger(loggerobj, loggername, printline = False):
     """ This is a very basic logger, which logs every Level and writes the time
     and a Message.
     """
-    logger = logging.getLogger('cocktail_application')
+    logger = logging.getLogger(loggerobj)
     logger.setLevel(logging.DEBUG)
-    name_ = "Logfile_{}.log".format(loggername)
+    name_ = "{}.log".format(loggername)
     fh = logging.FileHandler(name_)
     fh.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
@@ -17,5 +20,44 @@ def basiclogger(loggername):
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     # Logging the Start of the Programm. If it runs a whole evening, each restart means a previous crash.
-    template = "{:-^80}"
-    logger.debug(template.format("Restarting the Programm",))
+    if printline:
+        template = "{:-^80}"
+        logger.debug(template.format("Restarting the Programm",))
+
+
+def logfunction(func):
+    """ Logs every time the function name and time needed """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if globals.decoactivate:
+            trigger = True
+            logger_t = logging.getLogger('timing')
+            t1 = time.time()
+            try:
+                func(*args, **kwargs)
+            except Exception:
+                logger_t.exception("The function {} could not be fully excecuted!".format(func.__name__))
+                trigger = False
+                print("The function {} could not be fully excecuted!".format(func.__name__))
+            t2 = round((time.time() - t1) * 1000)
+            if trigger:
+                logger_t.info("The function {} took {} ms to complete".format(func.__name__, t2))
+        else:
+            func(*args, **kwargs)
+    return wrapper
+
+def logerror(func):
+    """ Logs every time an error occours """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if globals.decoactivate:
+            logger_t = logging.getLogger('timing')
+            try:
+                func(*args, **kwargs)
+            except Exception:
+                logger_t.exception
+                logger_t.exception("The function {} could not be fully excecuted!".format(func.__name__))
+                print("The function {} could not be fully excecuted!".format(func.__name__))
+        else:
+            func(*args, **kwargs)
+    return wrapper
