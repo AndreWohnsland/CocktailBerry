219c3d90e0fe6e183cd693d0caf5c599719efdfd
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-12-20 14:51:48 +0100

    Added microservice app

---
+++ microservice/app.py
@@ -0,0 +1,35 @@
+#!flask/bin/python
+from flask import Flask, request, abort, jsonify
+import os
+import requests
+import json
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return jsonify({"status": "api working"})
+
+
+@app.route("/hookhandler/cocktail", methods=["POST"])
+def post_cocktail_hook():
+    if not request.json or not "cocktailname" in request.json:
+        abort(400)
+    cocktail = {
+        "cocktailname": request.json["cocktailname"],
+        "makedate": datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+    }
+    url = os.getenv("HOOK_ENDPOINT")
+    headers = {"content-type": "application/json"}
+    req = requests.post(url, data=json.dumps(cocktail), headers=headers)
+    print(cocktail)
+    return jsonify({"cocktail": cocktail, "poststatus": req.status_code}), 201
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.getenv("PORT"))
