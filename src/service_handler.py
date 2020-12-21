855a307efa94983daf1453920bf8c5d5f2fac30e
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-12-21 08:46:23 +0100

    Introduced service handler for calls

---
+++ src/service_handler.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from typing import Dict
+from config.config_manager import ConfigManager
+from src.logger_handler import LoggerHandler
+
+
+class ServiceHandler(ConfigManager):
+    """Class to handle all calls to the mircoservice within the docker"""
+
+    def __init__(self):
+        self.base_url = self.MICROSERVICE_BASE_URL
+        self.logger = LoggerHandler("microservice", "service_logs")
+        self.headers = {"content-type": "application/json"}
+
+    def log_connection_error(self, func: str):
+        self.logger.log_event("ERROR", f"Could not connect to the microservice endpoint: '{func}'")
+
+    def return_service_disabled(self):
+        return {
+            "status": 503,
+            "message": "microservice disabled",
+        }
+
+    def post_cocktail_to_hook(self, cocktailname: str, cocktail_volume: int) -> Dict:
+        if not self.USE_MICROSERVICE:
+            return self.return_service_disabled()
+        # calculare volume in litre
+        payload = json.dumps({"cocktailname": cocktailname, "volume": cocktail_volume / 1000})
+        endpoint = "/hookhandler/cocktail"
+        full_url = f"{self.base_url}{endpoint}"
+        ret_data = {}
+        try:
+            req = requests.post(full_url, data=payload, headers=self.headers)
+            message = str(req.text).replace("\n", "")
+            ret_data = {
+                "status": req.status_code,
+                "message": message,
+            }
+            self.logger.log_event("INFO", f"Posted cocktail to {full_url} | {req.status_code}: {message}")
+        except requests.exceptions.ConnectionError:
+            self.log_connection_error(full_url)
+        return ret_data
