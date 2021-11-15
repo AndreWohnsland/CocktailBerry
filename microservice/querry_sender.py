867fd39d13f5c0bbc0a0e5d8ae25886790d12b0f
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-11-15 09:43:03 +0100

    Used logging instead of print

--- microservice/querry_sender.py
+++ microservice/querry_sender.py
@@ -1,9 +1,10 @@
 import os
 import requests
+from flask import Flask
 from database import DatabaseHandler
 
 
-def try_send_querry_data():
+def try_send_querry_data(app: Flask):
     db_handler = DatabaseHandler()
     failed_data = db_handler.get_failed_data()
     headers = {"content-type": "application/json"}
@@ -12,11 +13,11 @@ def try_send_querry_data():
     if not failed_data:
         return
     # Else try to send all remaining data
-    print("Got some not send data, trying to send ...")
+    app.logger.info("Found some not sended data, trying to send ...")
     for send_id, data in failed_data:
         try:
             res = requests.post(url, data=data, headers=headers)
-            print(f"Code: {res.status_code}, Payload: {data}")
+            app.logger.info(f"Code: {res.status_code}, Payload: {data}")
         except requests.exceptions.ConnectionError:
             return
         # if send successfully, delete this entry
