c855c51a724fb92e34b15ebc51b61d88d0771d9e
Andre Wohnsland <Andre_Wohnsland@web.de>
2021-11-25 19:40:44 +0100

    Added word / line wrap for button

--- src_ui/setup_team_window.py
+++ src_ui/setup_team_window.py
@@ -26,7 +26,30 @@ class TeamScreen(QDialog, Ui_Teamselection, ConfigManager):
         self.move(0, 0)
         UI_LANGUAGE.adjust_team_window(self)
         DP_CONTROLLER.set_display_settings(self)
+        self.__set_icon_text_breaks()
 
     def set_team(self, team: str):
         shared.selected_team = team
         self.close()
+
+    def __set_icon_text_breaks(self):
+        """Adds new lines if neccecary to team button labels"""
+        # ~ 12 upper chars / 400 px
+        width = self.PBteamone.frameGeometry().width()
+        self.PBteamone.setText(self.__split_team_names(self.TEAM_BUTTON_NAMES[0], width))
+        self.PBteamtwo.setText(self.__split_team_names(self.TEAM_BUTTON_NAMES[1], width))
+
+    def __split_team_names(self, name, width):
+        """Pseudo line wrap, since its not available for button"""
+        singles = name.split(" ")
+        label = singles[0]
+        cur_len = len(label)
+        for sing in singles[1::]:
+            added_len = len(sing)
+            if width / (cur_len + added_len + 1) <= 25:
+                label += f"\n{sing}"
+                cur_len = added_len
+            else:
+                cur_len += added_len
+                label += f" {sing}"
+        return label
