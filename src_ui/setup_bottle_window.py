70686b892e870044807fc06ac500a7d62223b425
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2021-12-04 09:25:58 +0100

    Dynamic removal of bottle count to config

--- src_ui/setup_bottle_window.py
+++ src_ui/setup_bottle_window.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow
+from config.config_manager import ConfigManager
 
 from ui_elements.bottlewindow import Ui_Bottlewindow
 
@@ -8,12 +9,13 @@ from src.dialog_handler import UI_LANGUAGE
 from src.display_controller import DP_CONTROLLER
 
 
-class BottleWindow(QMainWindow, Ui_Bottlewindow):
+class BottleWindow(QMainWindow, Ui_Bottlewindow, ConfigManager):
     """ Creates the Window to change to levels of the bottles. """
 
     def __init__(self, parent=None):
         """ Init. Connects all the buttons, gets the names from Mainwindow/DB. """
         super().__init__()
+        ConfigManager.__init__(self)
         self.setupUi(self)
         # connects all the buttons
         self.PBAbbrechen.clicked.connect(self.abbrechen_clicked)
@@ -26,17 +28,31 @@ class BottleWindow(QMainWindow, Ui_Bottlewindow):
         self.maxvolume = []
         self.asign_bottle_data()
         # creates lists of the objects and assings functions later through a loop
+        number = self.__choose_bottle_number()
         myplus = [getattr(self, f"PBMplus{x}") for x in range(1, 11)]
         myminus = [getattr(self, f"PBMminus{x}") for x in range(1, 11)]
         mylabel = [getattr(self, f"LAmount{x}") for x in range(1, 11)]
-        for plus, minus, field, vol in zip(myplus, myminus, mylabel, self.maxvolume):
+        myname = [getattr(self, f"LName{x}") for x in range(1, 11)]
+
+        #  since zip only goes to the minimal of all, only one [:number] is needed
+        for plus, minus, field, vol in zip(myplus, myminus, mylabel[:number], self.maxvolume):
             plus.clicked.connect(lambda _, l=field, b=vol: DP_CONTROLLER.plusminus(
                 label=l, operator="+", minimal=50, maximal=b, delta=25))
             minus.clicked.connect(lambda _, l=field, b=vol: DP_CONTROLLER.plusminus(
                 label=l, operator="-", minimal=50, maximal=b, delta=25))
+
+        # remove the elements exceeding the bottle number
+        for elements in [myplus, myminus, mylabel, myname]:
+            for element in elements[number::]:
+                element.deleteLater()
+
         UI_LANGUAGE.adjust_bottle_window(self)
         self.showFullScreen()
         DP_CONTROLLER.set_display_settings(self)
+
+    def __choose_bottle_number(self):
+        """Selects the number of Bottles in the bottles tab, all is ten"""
+        return min(self.MAKER_NUMBER_BOTTLES, 10)
 
     def abbrechen_clicked(self):
         """ Closes the Window without a change. """
@@ -44,7 +60,8 @@ class BottleWindow(QMainWindow, Ui_Bottlewindow):
 
     def eintragen_clicked(self):
         """ Enters the Data and closes the window. """
-        labelname = [getattr(self, f"LAmount{i}") for i in range(1, 11)]
+        number = self.__choose_bottle_number()
+        labelname = [getattr(self, f"LAmount{i}") for i in range(1, number + 1)]
         for label, ingredient_id, maxvolume in zip(labelname, self.id_list, self.maxvolume):
             new_amount = min(int(label.text()), maxvolume)
             DB_COMMANDER.set_ingredient_level_to_value(ingredient_id, new_amount)
@@ -52,7 +69,8 @@ class BottleWindow(QMainWindow, Ui_Bottlewindow):
         self.close()
 
     def asign_bottle_data(self):
-        bottle_data = DB_COMMANDER.get_bottle_data_bottle_window()
+        number = self.__choose_bottle_number()
+        bottle_data = DB_COMMANDER.get_bottle_data_bottle_window()[:number]
         for i, (ingredient_name, bottle_level, ingredient_id, ingredient_volume) in enumerate(bottle_data, start=1):
             labelobj = getattr(self, f"LName{i}")
             labelname = getattr(self, f"LAmount{i}")
