7eb3698bf36004445fc9ff40a8298cb771805bdf
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2021-12-28 14:53:43 +0100

    Using migrator at program start

--- runme.py
+++ runme.py
@@ -3,11 +3,14 @@ from PyQt5.QtWidgets import QApplication
 
 from src.error_handler import logerror
 from src.config_manager import ConfigManager
+from src.migrator import Migrator
 from src_ui.setup_mainwindow import MainScreen
 
 
 @logerror
 def main():
+    migrator = Migrator()
+    migrator.make_migrations()
     c_manager = ConfigManager()
     c_manager.sync_config_to_file()
     app = QApplication(sys.argv)
