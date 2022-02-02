1d1922cb83d59b0fb64ad7fd5111176020caaee6
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-02-02 20:43:40 +0100

    Adjusted launcher for new dashboard

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,10 @@
 # uncomment / comment the according lines
 
 # launcher.sh for dashboard
-# no need for sudo if there were no Numpy import errors
-# sudo python3 /home/pi/Cocktailmaker_AW/dashboard/qt-app/main.py
+# This for qt-app (recommended way)
+# python3 /home/pi/Cocktailmaker_AW/dashboard/qt-app/main.py
+# This for Dash (WebApp, advanced way)
+# gunicorn --workers=5 --threads=1 -b :8050 index:server 
 
 # launcher.sh for cocktailmaker
 python3 /home/pi/Cocktailmaker_AW/runme.py
