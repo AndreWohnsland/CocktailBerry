6b8f1a9e7c99cb8a1d8f82d1d0c0484a1c986754
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-03-15 21:28:21 +0100

    Added helper for url / header gen

---
+++ microservice/helper.py
@@ -0,0 +1,21 @@
+import os
+from typing import Dict, List, Tuple
+
+DEFAULT_HOOK_EP = "enpointforhook"
+DEFAULT_API_KEY = "readdocshowtoget"
+API_ENDPOINT = "https://cberry.deta.dev/cocktail"
+
+
+def generate_headers_and_urls() -> Tuple[Dict, List[str]]:
+    """Generates the header as well as the urls from the .env data"""
+    hookurl = os.getenv("HOOK_ENDPOINT")
+    api_key = os.getenv("API_KEY")
+    headers = {
+        "content-type": "application/json",
+        "X-API-Key": api_key,
+    }
+    use_hook = hookurl != DEFAULT_HOOK_EP
+    use_api = api_key != DEFAULT_API_KEY
+    urls = ([hookurl] if use_hook else []) + ([API_ENDPOINT] if use_api else [])
+
+    return headers, urls
