2931adffeafa47c7fc289dbca53339a8c4abe206
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-04-20 17:00:02 +0200

    added bavaria style

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ PROJECT_NAME = "CocktailBerry"
 MAX_SUPPORTED_BOTTLES = 16
 SUPPORTED_LANGUAGES = ["en", "de"]
 SUPPORTED_BOARDS = ["RPI"]
-SUPPORTED_THEMES = ["default"]
+SUPPORTED_THEMES = ["default", "bavaria"]
