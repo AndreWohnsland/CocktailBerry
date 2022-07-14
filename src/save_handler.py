c4de8aa9235ae8698d43c2973b5fe976852c6744
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-07-14 17:35:02 +0200

    Switched to weak private

--- src/save_handler.py
+++ src/save_handler.py
@@ -8,8 +8,8 @@ from src.display_controller import DP_CONTROLLER
 from src.logger_handler import LoggerHandler
 from src.service_handler import SERVICE_HANDLER
 
-DIRPATH = Path(__file__).parent.absolute()
-logger = LoggerHandler("save_handler", "production_logs")
+_DIRPATH = Path(__file__).parent.absolute()
+_logger = LoggerHandler("save_handler", "production_logs")
 
 
 class SaveHandler:
@@ -19,9 +19,9 @@ class SaveHandler:
             DP_CONTROLLER.say_wrong_password()
             return
         consumption_list = DB_COMMANDER.get_consumption_data_lists_ingredients()
-        self.save_quant("Zutaten_export.csv", consumption_list)
+        self._save_quant("Zutaten_export.csv", consumption_list)
         DB_COMMANDER.delete_consumption_ingredients()
-        logger.log_event("INFO", "Ingredient data was exported")
+        _logger.log_event("INFO", "Ingredient data was exported")
 
     def export_recipes(self, w):
         """Export the recipes to a csv file, resets count"""
@@ -29,22 +29,22 @@ class SaveHandler:
             DP_CONTROLLER.say_wrong_password()
             return
         consumption_list = DB_COMMANDER.get_consumption_data_lists_recipes()
-        self.save_quant("Rezepte_export.csv", consumption_list)
+        self._save_quant("Rezepte_export.csv", consumption_list)
         DB_COMMANDER.delete_consumption_recipes()
-        logger.log_event("INFO", "Recipe data was exported")
+        _logger.log_event("INFO", "Recipe data was exported")
 
-    def save_quant(self, filename: str, data: List):
+    def _save_quant(self, filename: str, data: List):
         """ Saves all the amounts of the ingredients/recipes to a csv and reset the counter to zero"""
-        self.write_rows_to_csv(filename, [*data, [" "]])
+        self._write_rows_to_csv(filename, [*data, [" "]])
         DP_CONTROLLER.say_all_data_exported()
 
-    def write_rows_to_csv(self, filename: str, data_rows: List):
+    def _write_rows_to_csv(self, filename: str, data_rows: List):
         """Write the data to the csv file, activate servicehandler for mail"""
         dtime = str(datetime.date.today())
         dtime = dtime.replace("-", "")
         subfoldername = "saves"
         full_file_name = f"{dtime}_{filename}"
-        savepath = DIRPATH.parent / subfoldername / full_file_name
+        savepath = _DIRPATH.parent / subfoldername / full_file_name
         with open(savepath, mode="a", newline="", encoding="utf-8") as writer_file:
             csv_writer = csv.writer(writer_file, delimiter=",")
             for row in data_rows:
