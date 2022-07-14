ffd48ca8e732865e4abe7b61079dea89dffe18fa
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-07-14 17:33:24 +0200

    Using default json header type

--- microservice/helper.py
+++ microservice/helper.py
@@ -12,10 +12,13 @@ def generate_urls_and_headers() -> List[Tuple[str, Dict[str, str]]]:
     api_key = os.getenv("API_KEY")
     hook_headers_config = os.getenv("HOOK_HEADERS", None)
     if hook_headers_config is None:
-        hook_headers = {}
+        # If no headers are set, use common content type
+        hook_headers = {
+            "content-type": "application/json"
+        }
     else:
         # Split the header config into a list
-        # the config is header=value,header2=value2
+        # the config is header:value,header2:value2
         headers = hook_headers_config.split(",")
         hook_headers = dict([x.split(":") for x in headers])
     api_headers = {
