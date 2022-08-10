31996054564ddcd5effe193f0dcac63408b3c060
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-08-10 13:54:21 +0200

    Adjusted bold style where needed

--- src/ui_elements/scaleabletab.py
+++ src/ui_elements/scaleabletab.py
@@ -1,5 +1,5 @@
-from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QTabWidget, QTabBar
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QTabWidget, QTabBar
 
 
 class ScaleableTabBar(QTabBar):
