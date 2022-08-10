3a5f0aa472eed14f8832647b6ce24a673a2205b7
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-08-10 17:47:31 +0200

    Adjusted path and docs

--- scripts/compile_ui_to_python.ps1
+++ scripts/compile_ui_to_python.ps1
@@ -1,3 +1,5 @@
+cd .\src\ui_elements\
+
 $files = @("available", "bonusingredient", "bottlewindow", "Cocktailmanager_2", "customdialog", "handadds", "Keyboard", "optionwindow", "passwordbuttons2", "progressbarwindow", "teamselection")
 
 foreach ($f in $files) {
