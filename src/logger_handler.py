4b4dbbfab33f307dda58491e3bb5cef7b99fe370
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-11-06 08:31:06 +0100

    Using dataclass as default

--- src/logger_handler.py
+++ src/logger_handler.py
@@ -23,7 +23,7 @@ class LoggerHandler:
 
     log_folder = _DIRPATH.parent / "logs"
 
-    def __init__(self, loggername: str, filename: str = "production_logs"):
+    def __init__(self, loggername: str, filename: str = LogFiles.PRODUCTION):
         self.loggername = loggername
         self.path = LoggerHandler.log_folder / f"{filename}.log"
 
