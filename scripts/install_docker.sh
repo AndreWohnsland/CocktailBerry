0b7353bfee7339acfa8cd6de62e3b6beda135c7d
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2022-11-23 21:10:59 +0100

    using compose v2

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -6,6 +6,5 @@ sudo apt-get update && sudo apt-get -y upgrade
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ${USER}
 sudo su - ${USER}
-sudo apt-get install libffi-dev libssl-dev
-sudo pip3 install docker-compose
+sudo apt-get install docker-compose-plugin
 sudo systemctl enable docker
