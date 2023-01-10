8d588c02e62a4c1cdd6763931eb4eb1d42b860cb
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-01-10 19:45:21 +0100

    Added berry style

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,6 @@ PROJECT_NAME = "CocktailBerry"
 MAX_SUPPORTED_BOTTLES = 16
 SUPPORTED_LANGUAGES = ["en", "de"]
 SUPPORTED_BOARDS = ["RPI"]
-SUPPORTED_THEMES = ["default", "bavaria", "alien"]
+SUPPORTED_THEMES = ["default", "bavaria", "alien", "berry"]
 NEEDED_PYTHON_VERSION = (3, 7)
 FUTURE_PYTHON_VERSION = (3, 9)
