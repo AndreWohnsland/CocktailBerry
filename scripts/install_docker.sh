7bb0e19c8444851ee8cc2b8459350ee52068ff40
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-01-19 18:07:28 +0100

    Adjusted docker install script

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 echo "Installing Docker"
-# Installing Docker
 sudo apt-get update && sudo apt-get -y upgrade
-curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker ${USER}
-sudo su - ${USER}
-sudo apt-get install docker-compose-plugin
-sudo systemctl enable docker
+sudo apt install docker.io -y
+docker --version || echo "Docker installation failed :("
+echo "Adds current user to docker permissions"
+sudo usermod -aG docker $USER
+newgrp docker
+echo "Installing Compose"
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-aarch64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version || echo "Compose installation failed :("
+
+# This still seem not to work on rpi
+# sudo apt-get install docker-compose-plugin
