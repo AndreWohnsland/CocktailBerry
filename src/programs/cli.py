7c1fcf423c77d1ec03967c968d8cef67e123baef
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-01-26 18:44:49 +0100

    Added cli clear db command

--- src/programs/cli.py
+++ src/programs/cli.py
@@ -9,6 +9,7 @@ from src.migration.update_data import add_new_recipes_from_default_db
 from src.programs.cocktailberry import run_cocktailberry
 from src.programs.calibration import run_calibration
 from src.programs.data_import import importer
+from src.programs.clearing import clear_local_database
 
 
 cli = typer.Typer(add_completion=False)
@@ -50,7 +51,8 @@ def dataimport(
     If the units are not in ml, please provide the conversion factor into ml.
 
     The file should contain the cocktail name, followed by ingredient data (amount, name).
-    For further information regarding the file structure, please see https://cocktailberry.readthedocs.io/advanced/#importing-recipes-from-file.
+    For further information regarding the file structure,
+    please see https://cocktailberry.readthedocs.io/advanced/#importing-recipes-from-file.
     """
     importer(path, conversion, no_unit)
 
@@ -61,7 +63,8 @@ def update_database():
     Using the default provided database to check for newly added recipes due to new updates.
     Adds the new recipes including missing ingredients to the local database.
     Ignore recipes that collide with names of your self-added recipes.
-    Creates a backup before doing the update, see also https://cocktailberry.readthedocs.io/troubleshooting/#restoring-database
+    Creates a backup before doing the update,
+    see also https://cocktailberry.readthedocs.io/troubleshooting/#restoring-database
 
     Please take note that the ingredients are in german, so if you renamed your ingredients,
     this will most likely add all ingredients from the new recipes in german to your local database.
@@ -70,3 +73,16 @@ def update_database():
     More information also at https://cocktailberry.readthedocs.io/advanced/#updating-local-database
     """
     add_new_recipes_from_default_db()
+
+
+@cli.command()
+def clear_database():
+    """
+    Clears the local database from the entered or default data.
+    After this action, there will be no recipes or ingredients in your local CocktailBerry data.
+    A backup of your local database is created before deleting.
+    Use this if you want to build your own custom database and not use any of the supplied data.
+    See also: LINK
+    """
+    # TODO: LINK
+    clear_local_database()
