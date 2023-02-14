43b4de7cbe3159f7c920882c8d1b0e46b4e5a38e
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-02-14 16:01:18 +0100

    Use inverted outside interface

--- src/machine/generic_board.py
+++ src/machine/generic_board.py
@@ -17,17 +17,18 @@ class GenericController(PinController):
     """Controller class to control pins on a generic board"""
 
     def __init__(self, inverted: bool) -> None:
-        super().__init__(inverted)
+        super().__init__()
+        self.inverted = inverted
         self.devenvironment = DEV
         self.low = False
         self.high = True
         if inverted:
             self.low, self.high = self.high, self.low
         self.gpios: dict[int, GPIO] = {}
+        print(f"Devenvironment on the Generic Pin Control module is {'on' if self.devenvironment else 'off'}")
 
     def initialize_pin_list(self, pin_list: List[int]):
         """Set up the given pin list"""
-        print(f"Devenvironment on the Generic Pin Control module is {'on' if self.devenvironment else 'off'}")
         init_value = "high" if self.inverted else "out"
         if not self.devenvironment:
             for pin in pin_list:
--- src/machine/raspberry.py
+++ src/machine/raspberry.py
@@ -19,20 +19,21 @@ class RpiController(PinController):
     """Controller class to control Raspberry Pi pins"""
 
     def __init__(self, inverted: bool) -> None:
-        super().__init__(inverted)
+        super().__init__()
+        self.inverted = inverted
         self.devenvironment = DEV
         self.low = GPIO.LOW if not DEV else 0
         self.high = GPIO.HIGH if not DEV else 1
         if inverted:
             self.low, self.high = self.high, self.low
+        print(f"Devenvironment on the RPi module is {'on' if self.devenvironment else 'off'}")
 
     def initialize_pin_list(self, pin_list: List[int]):
         """Set up the given pin list"""
-        print(f"Devenvironment on the RPi module is {'on' if self.devenvironment else 'off'}")
         if not self.devenvironment:
             GPIO.setup(pin_list, GPIO.OUT, initial=self.low)
         else:
-            logger.log_event("WARNING", "Could not import RPi.GPIO. Will not be able to control pins")
+            logger.log_event("WARNING", f"Could not import RPi.GPIO. Will not be able to control pins: {pin_list}")
 
     def activate_pin_list(self, pin_list: List[int]):
         """Activates the given pin list"""
