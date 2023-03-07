afb3aa3febb79fc09e608612e0b6f49f6782e057
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-07 17:44:56 +0100

    Let led circle two rounds per second

--- src/machine/leds.py
+++ src/machine/leds.py
@@ -127,7 +127,8 @@ class _controllableLED(_LED):
 
     def _preparation_thread(self):
         """Fills one by one with same random color, then repeats / overwrites old ones"""
-        wait_ms = 40
+        # Make the circle / dot approximate 2 rounds per second
+        wait_ms = 500 / cfg.LED_COUNT
         self.turn_on(Color(randint(0, 255), randint(0, 255), randint(0, 255)))
         while self.is_preparing:
             color = Color(
