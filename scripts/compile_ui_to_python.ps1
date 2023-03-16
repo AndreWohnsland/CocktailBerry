a2bb2d2cb0640b641487d1764e6ae54adca867cc
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-16 18:20:05 +0100

    Add wifi window ui

--- scripts/compile_ui_to_python.ps1
+++ scripts/compile_ui_to_python.ps1
@@ -1,6 +1,13 @@
 cd .\src\ui_elements\
 
-$files = @("available", "bonusingredient", "bottlewindow", "cocktailmanager", "calibration", "customdialog", "datepicker", "handadds", "keyboard", "optionwindow", "numpad", "progressbarwindow", "teamselection", "passworddialog", "customprompt", "logwindow", "rfidwriter")
+$files = @(
+  "available", "bonusingredient", "bottlewindow",
+  "cocktailmanager", "calibration", "customdialog",
+  "datepicker", "handadds", "keyboard",
+  "optionwindow", "numpad", "progressbarwindow",
+  "teamselection", "passworddialog", "customprompt",
+  "logwindow", "rfidwriter", "wifiwindow"
+)
 
 foreach ($f in $files) {
   pyuic5 -x .\$f.ui -o .\$f.py
