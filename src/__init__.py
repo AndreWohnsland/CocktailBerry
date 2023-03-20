8093af4f5c513abf6b2de96c2b718e02940a7781
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-20 17:42:46 +0100

    Phase out picoodev until next patch

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,6 @@ MAX_SUPPORTED_BOTTLES = 16
 SupportedLanguagesType = Literal["en", "de"]
 SupportedBoardType = Literal["RPI", "Generic"]
 SupportedThemesType = Literal["default", "bavaria", "alien", "berry", "custom"]
-SupportedRfidType = Literal["No", "PiicoDev", "MFRC522"]
+SupportedRfidType = Literal["No", "MFRC522"]  # "PiicoDev"
 NEEDED_PYTHON_VERSION = (3, 9)
 FUTURE_PYTHON_VERSION = (3, 9)
