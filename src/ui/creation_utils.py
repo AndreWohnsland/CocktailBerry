122d28559adee0b2f1b1526b5f00b4a3c0c4ca82
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-25 08:56:05 +0100

    Add button creation

--- src/ui/creation_utils.py
+++ src/ui/creation_utils.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QSpacerItem, QSizePolicy
+from typing import Optional
+from PyQt5.QtWidgets import QWidget, QSpacerItem, QSizePolicy, QPushButton
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QSize
 
 SMALL_FONT = 14
 MEDIUM_FONT = 16
@@ -19,3 +21,11 @@ def adjust_font(element: QWidget, font_size: int, bold: bool = False):
 def create_spacer(height: int, width: int = 20):
     """Creates a spacer of given height and optional width"""
     return QSpacerItem(width, height, QSizePolicy.Minimum, QSizePolicy.Fixed)  # type: ignore
+
+
+def create_button(label: str, parent: Optional[QWidget] = None):
+    btn = QPushButton(label, parent)
+    btn.setMaximumSize(QSize(16777215, 200))
+    btn.setMinimumSize(QSize(0, 70))
+    adjust_font(btn, LARGE_FONT, True)
+    return btn
