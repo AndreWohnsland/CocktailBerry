2f020948af0782b6ea767911a232d4a0b48600ff
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-26 08:58:47 +0200

    Created skeleton for addons

---
+++ src/programs/addon_skeleton.py
@@ -0,0 +1,55 @@
+from typing import Callable
+from PyQt5.QtWidgets import QVBoxLayout
+
+# Auto created by CocktailBerry CLI
+# Imports are automatically generated for all the examples from the docs
+# You can delete the imports you don't need
+# For more information see: https://cocktailberry.readthedocs.io/addons/
+
+# Use the cfg to add your config / validation
+from src.config_manager import CONFIG as cfg, ConfigError
+# Use the uil to add description and according translation
+from src.dialog_handler import UI_LANGUAGE as uil
+# Use the LoggerHandler class for your logger
+from src.logger_handler import LoggerHandler
+# Use the dpc to display dialogues or prompts to the user
+from src.display_controller import DP_CONTROLLER as dpc
+# You can access the default database with help of the dbc
+from src.database_commander import DB_COMMANDER as dbc
+# The addon interface will provide intellisense for all possible methods
+from src.programs.addons import AddonInterface
+
+ADDON_NAME = "ADDON_NAME_HOLDER"
+_logger = LoggerHandler("ADDON: ADDON_NAME_HOLDER")
+
+
+# The class needs to be called Addon and inherit from the AddonInterface
+class Addon(AddonInterface):
+    def setup(self):
+        """Inits the addon, executed at program start. """
+
+    def cleanup(self):
+        """Method for cleanup, executed a program end. """
+
+    def before_cocktail(self):
+        """Executed right before the cocktail preparation.
+        In case of a RuntimeError, the cocktail will not be prepared
+        and the message will be shown to the user.
+        """
+
+    def after_cocktail(self):
+        """Executed right after the cocktail preparation"""
+
+    def build_gui(
+        self,
+        container: QVBoxLayout,
+        button_generator: Callable[[str, Callable[[], None]], None]
+    ) -> bool:
+        """Builds up the GUI to do additional things on command.
+        Return:
+        True, if you want to build an interface / GUI
+        False, if you don't provide an interface / GUI
+        """
+        # Change to True, if you build your own GUI
+        # Otherwise, an information will be shown, that the addon do not provide a GUI
+        return False
