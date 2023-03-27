77a202c7497311b403f3322fa9eb87211ac4acd4
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-27 10:26:44 +0200

    Make compose installation compatible for 32 bit

--- scripts/install_compose.sh
+++ scripts/install_compose.sh
@@ -3,6 +3,12 @@
 echo "Installing Compose"
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-aarch64 -o ~/.docker/cli-plugins/docker-compose
+if $(uname -m | grep '64'); then
+  echo "Detected 64 bit system, using aarch64 compose image"
+  curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+else
+  echo "Detected 32 bit system, using armv7l compose image"
+  curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-armv7 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+fi
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 docker compose version || echo "Compose installation failed :("
