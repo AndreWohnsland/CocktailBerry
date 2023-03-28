2d7cddd66342f3e26a81cf0ae85c828c5862b185
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-03-28 10:45:10 +0200

    Test for not breaking current terminal

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -6,7 +6,8 @@ sudo apt install docker.io -y
 docker --version || echo "Docker installation failed :("
 echo "Adds current user to docker permissions"
 sudo usermod -aG docker $USER
-newgrp docker
+sudo su - $USER
+# newgrp docker
 
 # This still seem not to work on rpi
 # sudo apt-get install docker-compose-plugin
