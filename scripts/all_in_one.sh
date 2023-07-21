3af7456dc5d6bfa8f7b0e8eb69ff322ca64eb430
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-07-21 15:34:28 +0200

    Add richer output to script

--- scripts/all_in_one.sh
+++ scripts/all_in_one.sh
@@ -3,11 +3,11 @@
 
 # Welcome and system updates
 echo "~~~~~~~ CocktailBerry All In One Installation Script v1 ~~~~~~~"
-echo "Updating system to latest version, depending on your system age, this may take some time ..."
+echo "~ Updating system to latest version, depending on your system age, this may take some time ... ~"
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Steps for git
-echo "Check if git is installed"
+echo "~ Check if git is installed ~"
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE -ne 0 ]; then
@@ -18,11 +18,11 @@ else
 fi
 
 # also link python to python3 if its still an old system
-echo "linking python to python3 if not already done"
+echo "~ linking python to python3 if not already done ~"
 sudo apt install python-is-python3
 
 # steps for python >= 3.9
-echo "Check that Python version is at least 3.9"
+echo "~ Check that Python version is at least 3.9 ~"
 version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
 parsedVersion=$(echo "${version//./}")
 echo "Detected version: $version"
@@ -44,29 +44,29 @@ if [[ "$DEBIAN_VERSION" -lt "11" ]]; then
 fi
 
 # Now gets CocktailBerry source
-echo "Getting the CocktailBerry project from GitHub ..."
+echo "~ Getting the CocktailBerry project from GitHub ... ~"
 echo "It will be located at ~/CocktailBerry"
 cd ~
 git clone https://github.com/AndreWohnsland/CocktailBerry.git
 cd ~/CocktailBerry
 
 # Do Docker related steps
-echo "Setting things up for Docker and Compose"
+echo "~ Setting things up for Docker and Compose ~"
 bash scripts/install_docker.sh -n
 cd ~/CocktailBerry
 bash scripts/install_compose.sh
 
 # Now we can finally set the program up ^-^
 cd ~/CocktailBerry
-echo "Setting up and installing CocktailBerry"
+echo "~ Setting up and installing CocktailBerry ~"
 bash scripts/setup.sh
 
-echo "Register successful installation: "
+echo ~ "Register successful installation: ~"
 OS_INFO=$(sed -nr 's/^PRETTY_NAME="(.+)"/\1/p' /etc/os-release)
 curl -X 'POST' 'https://cocktailberryapi-1-u0613408.deta.app/public/installation' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{"os_version": "'"$OS_INFO"'"}'
 echo ""
 
-echo "Everything should be set now! Have fun with CocktailBerry :)"
+echo "~ Everything should be set now! Have fun with CocktailBerry :) ~"
 echo "Made by Andre Wohnsland and contributors with <3"
 echo "Documentation is found at: https://cocktailberry.readthedocs.io/"
 echo "Source code at: https://github.com/AndreWohnsland/CocktailBerry"
