8aade5a4c59a665b3dab0adc60f0810795a8ea43
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2023-10-27 16:29:57 +0200

    Using lastversion for compose version

--- scripts/install_compose.sh
+++ scripts/install_compose.sh
@@ -3,12 +3,34 @@
 echo "Installing Compose"
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-if $(uname -m | grep '64'); then
+# in the future, sudo apt install docker-compose-plugin should work
+# sadly, this still does not work :(
+
+# we gonna use a virtual environment because thats the way on bookworm
+# and use lastversion to get the latest version url from the docker compose GitHub
+# This is used to have always the latest version and not a fixed one
+echo "Creating temporary virtual environment for lastversion"
+python -m venv --system-site-packages ~/.env-compose
+source ~/.env-compose/bin/activate
+echo "Installing lastversion"
+pip install -q lastversion
+
+# You could get the version number and just use it in the URL string
+# COMPOSE_VERSION=$(lastversion docker/compose)
+# but its more robust to get the whole asset URL
+
+if [ $(getconf LONG_BIT) = "64" ]; then
   echo "Detected 64 bit system, using aarch64 compose image"
-  curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  DOWNLOAD_URL=$(lastversion --assets --filter '\-linux-aarch64(?!.sha256)' docker/compose)
 else
   echo "Detected 32 bit system, using armv7l compose image"
-  curl -SL https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-armv7 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  DOWNLOAD_URL=$(lastversion --assets --filter '\-linux-armv7(?!.sha256)' docker/compose)
 fi
+curl -SL $DOWNLOAD_URL -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 docker compose version || echo "Compose installation failed :("
+
+# remove venv at the end
+echo "remove temporary virtual environment"
+deactivate
+rm -rf ~/.env-compose
