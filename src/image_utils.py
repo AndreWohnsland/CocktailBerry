c9de17e88b93eb9f6c490425a9c5a423c1ff1ed7
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-01-02 11:22:26 +0100

    Also read in exif data and rotate picture

--- src/image_utils.py
+++ src/image_utils.py
@@ -53,6 +53,8 @@ def process_image(image_path: Union[str, bytes, Path], resize_size: int = 500) -
     # catch errors in file things
     except (FileNotFoundError, UnidentifiedImageError):
         return None
+    # first check if the image needs to be rotated
+    img = check_picture_orientation(img)
     # Calculate dimensions for cropping
     width, height = img.size
     if width > height:
@@ -71,6 +73,29 @@ def process_image(image_path: Union[str, bytes, Path], resize_size: int = 500) -
     img = img.resize((resize_size, resize_size), Image.LANCZOS)  # pylint: disable=E1101
     # always convert to rgb
     img = img.convert("RGB")
+    return img
+
+
+def check_picture_orientation(img: Image.Image) -> Image.Image:
+    # Check for EXIF orientation tag
+    exif_data = img.getexif()
+    orientation = exif_data.get(0x0112, 1)  # 0x0112 is the EXIF Orientation tag
+    # rotate use counter clockwise, exif is clockwise, so don't be confused
+    # Exif tags are, we currently only handle normal rotations here:
+    # 1: Horizontal (normal)
+    # 2: Mirror horizontal
+    # 3: Rotate 180 <- is the same in rotate
+    if orientation == 3:
+        img = img.rotate(180, expand=True)
+    # 4: Mirror vertical
+    # 5: Mirror horizontal and rotate 270 CW
+    # 6: Rotate 90 CW <- is 270 counter CW
+    elif orientation == 6:
+        img = img.rotate(270, expand=True)
+    # 7: Mirror horizontal and rotate 90 CW
+    # 8: Rotate 270 CW <- is 90 counter CW
+    elif orientation == 8:
+        img = img.rotate(90, expand=True)
     return img
 
 
