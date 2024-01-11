64a0559d6a1c4e386098ddb30d779613c403728a
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-01-11 18:34:30 +0100

    Add all team buttons

--- src/ui/setup_team_window.py
+++ src/ui/setup_team_window.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 from PyQt5.QtWidgets import QDialog
 
 from src.ui_elements.teamselection import Ui_Teamselection
@@ -5,6 +6,7 @@ from src.config_manager import CONFIG as cfg, shared
 from src.display_controller import DP_CONTROLLER
 from src.dialog_handler import UI_LANGUAGE
 from src.machine.rfid import RFIDReader
+from src.ui.creation_utils import create_button
 
 
 class TeamScreen(QDialog, Ui_Teamselection):
@@ -14,14 +16,17 @@ class TeamScreen(QDialog, Ui_Teamselection):
         super().__init__()
         self.setupUi(self)
         DP_CONTROLLER.initialize_window_object(self)
-        self.PBteamone.clicked.connect(lambda: self.set_team(cfg.TEAM_BUTTON_NAMES[0]))
-        self.PBteamone.setText(cfg.TEAM_BUTTON_NAMES[0])
-        self.PBteamtwo.clicked.connect(lambda: self.set_team(cfg.TEAM_BUTTON_NAMES[1]))
-        self.PBteamtwo.setText(cfg.TEAM_BUTTON_NAMES[1])
+        # iterate over all team names and add a button for each
+        team_colors = cycle(["btn-teamone", "btn-teamtwo"])
+        for team_name in cfg.TEAM_BUTTON_NAMES:
+            team_button = create_button(team_name, font_size=40, max_h=300, min_h=100, css_class=next(team_colors))
+            team_button.clicked.connect(lambda _, t=team_name: self.set_team(t))
+            self.button_container.addWidget(team_button)
+            setattr(self, f"button_{team_name}", team_button)
         self.mainscreen = parent
         UI_LANGUAGE.adjust_team_window(self)
         DP_CONTROLLER.set_display_settings(self)
-        self.__set_icon_text_breaks()
+        # self.__set_icon_text_breaks()
         self._rfid_reader = None
         # reset the team name, since this needs to be read over rfid
         shared.team_member_name = None
@@ -29,39 +34,21 @@ class TeamScreen(QDialog, Ui_Teamselection):
         if cfg.RFID_READER != "No":
             self._rfid_reader = RFIDReader()
             self._rfid_reader.read_rfid(self._write_rfid_value)
+        else:
+            # we do not need the person label, so remove it
+            self.person_label.deleteLater()
 
     def _write_rfid_value(self, text: str, _id: str):
         shared.team_member_name = text
         self.person_label.setText(text)
 
     def set_team(self, team: str):
+        print(f"Team {team} selected")
         shared.selected_team = team
         if self._rfid_reader is not None:
             self._rfid_reader.cancel_reading()
-        self.close()
+        # self.close()
 
     def __del__(self):
         if self._rfid_reader is not None:
             self._rfid_reader.cancel_reading()
-
-    def __set_icon_text_breaks(self):
-        """Adds new lines if necessary to team button labels"""
-        # ~ 12 upper chars / 400 px
-        width = self.PBteamone.frameGeometry().width()
-        self.PBteamone.setText(self.__split_team_names(cfg.TEAM_BUTTON_NAMES[0], width))
-        self.PBteamtwo.setText(self.__split_team_names(cfg.TEAM_BUTTON_NAMES[1], width))
-
-    def __split_team_names(self, name: str, width: int):
-        """Pseudo line wrap, since its not available for button"""
-        singles = name.split(" ")
-        label = singles[0]
-        cur_len = len(label)
-        for sing in singles[1::]:
-            added_len = len(sing)
-            if width / (cur_len + added_len + 1) <= 25:
-                label += f"\n{sing}"
-                cur_len = added_len
-            else:
-                cur_len += added_len
-                label += f" {sing}"
-        return label
