f8bac8c918ed9b0a35f67dc2ab3b570e4b361dbe
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-02-12 20:07:56 +0100

    Also add ingredient selection

--- src/ui/cocktail_view.py
+++ src/ui/cocktail_view.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QVBoxLayout, QWidget, QGridLayout, QFrame
@@ -15,6 +15,8 @@ from src.image_utils import find_cocktail_image
 from src.config_manager import CONFIG as cfg
 from src.ui.creation_utils import create_button
 from src.ui.icons import ICONS, PresetIcon
+from src.filepath import DEFAULT_COCKTAIL_IMAGE
+from src.dialog_handler import UI_LANGUAGE
 
 if TYPE_CHECKING:
     from src.ui.setup_mainwindow import MainScreen
@@ -27,22 +29,27 @@ N_COLUMNS = max(N_COLUMNS, 1)
 SQUARE_SIZE = int(cfg.UI_WIDTH / (N_COLUMNS * 1.17))
 
 
-def generate_image_block(cocktail: Cocktail, mainscreen: MainScreen):
+def generate_image_block(cocktail: Optional[Cocktail], mainscreen: MainScreen):
     """Generates a image block for the given cocktail"""
     # those factors are taken from calculations based on the old static values
     header_font_size = round(SQUARE_SIZE / 15.8)
     header_height = round(SQUARE_SIZE / 6.3)
+    single_ingredient_label = UI_LANGUAGE.get_translation("label_single_ingredient", "main_window")
+    name_label = cocktail.name if cocktail is not None else single_ingredient_label
     button = create_button(
-        cocktail.name, font_size=header_font_size, min_h=0, max_h=header_height,
+        name_label, font_size=header_font_size, min_h=0, max_h=header_height,
         max_w=SQUARE_SIZE, css_class="btn-inverted btn-half-top"
     )
-    if cocktail.virgin_available:
+    if cocktail is not None and cocktail.virgin_available:
         icon = ICONS.generate_icon(PresetIcon.virgin, ICONS.color.background)
         button.setIcon(icon)
         button.setIconSize(QSize(20, 20))
-    label = ClickableLabel(cocktail.name)
+    label = ClickableLabel(name_label)
     label.setProperty("cssClass", "cocktail-picture-view")
-    cocktail_image = find_cocktail_image(cocktail)
+    if cocktail is None:
+        cocktail_image = DEFAULT_COCKTAIL_IMAGE
+    else:
+        cocktail_image = find_cocktail_image(cocktail)
     pixmap = QPixmap(str(cocktail_image))
     label.setPixmap(pixmap)
     label.setScaledContents(True)
@@ -53,13 +60,17 @@ def generate_image_block(cocktail: Cocktail, mainscreen: MainScreen):
     layout.setSpacing(0)
     layout.addWidget(button)
     layout.addWidget(label)
-    # take care of the button overload thingy, otherwise the first element will be a bool
-    button.clicked.connect(
-        lambda _, c=cocktail: mainscreen.open_cocktail_selection(c)
-    )
-    label.clicked.connect(
-        lambda c=cocktail: mainscreen.open_cocktail_selection(c)
-    )
+    if cocktail is not None:
+        # take care of the button overload thingy, otherwise the first element will be a bool
+        button.clicked.connect(
+            lambda _, c=cocktail: mainscreen.open_cocktail_selection(c)
+        )
+        label.clicked.connect(
+            lambda c=cocktail: mainscreen.open_cocktail_selection(c)
+        )
+    else:
+        button.clicked.connect(mainscreen.open_ingredient_window)
+        label.clicked.connect(mainscreen.open_ingredient_window)
     return layout
 
 
@@ -92,6 +103,9 @@ class CocktailView(QWidget):
         cocktails = DB_COMMANDER.get_possible_cocktails()
         # sort cocktails by name
         cocktails.sort(key=lambda x: x.name.lower())
+        # add last "filler" element, this is for the single ingredient element
+        if cfg.MAKER_ADD_SINGLE_INGREDIENT:
+            cocktails = cocktails + [None]
         # fill the grid with N_COLUMNS columns, then go to another row
         for i in range(0, len(cocktails), N_COLUMNS):
             for j in range(N_COLUMNS):
