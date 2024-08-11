816aacf00520e0720841990d36ed96c382331c34
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-08-11 11:58:38 +0200

    Add callback on close to dialog window

--- src/ui/setup_custom_dialog.py
+++ src/ui/setup_custom_dialog.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Callable
+
 from PyQt5.QtWidgets import QMainWindow
 
 from src.dialog_handler import UI_LANGUAGE
@@ -8,13 +12,14 @@ from src.ui_elements.customdialog import Ui_CustomDialog
 class CustomDialog(QMainWindow, Ui_CustomDialog):
     """Class for the Team selection Screen."""
 
-    def __init__(self, message: str, title: str = "Information", use_ok=False):
+    def __init__(self, message: str, title: str = "Information", use_ok=False, close_callback: Callable | None = None):
         super().__init__()
         self.setupUi(self)
         DP_CONTROLLER.initialize_window_object(self)
         self.informationLabel.setText(message)
         self.setWindowTitle(title)
         self.closeButton.clicked.connect(self.close_clicked)
+        self.close_callback = close_callback
 
         # self.closeButton.setText(close_text)
         UI_LANGUAGE.adjust_custom_dialog(self, use_ok)
@@ -22,4 +27,6 @@ class CustomDialog(QMainWindow, Ui_CustomDialog):
         DP_CONTROLLER.set_display_settings(self)
 
     def close_clicked(self):
+        if self.close_callback is not None:
+            self.close_callback()
         self.close()
