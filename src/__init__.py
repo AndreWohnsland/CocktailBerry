e73b958eed60b2653b1672d24aefb13af7df7286
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-09-14 20:57:30 +0200

    Bump version

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-__version__ = "1.36.1"
+__version__ = "1.37.0"
 PROJECT_NAME = "CocktailBerry"
 MAX_SUPPORTED_BOTTLES = 24
 SupportedLanguagesType = Literal["en", "de"]
