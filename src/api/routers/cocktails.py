aee9e6777d6360b29670998fb620e999b3638047
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-10-20 14:12:58 +0200

    Add models an sse to api

--- src/api/routers/cocktails.py
+++ src/api/routers/cocktails.py
@@ -1,22 +1,43 @@
+import asyncio
+from typing import Optional
+
 from fastapi import APIRouter
+from sse_starlette.sse import EventSourceResponse
+
+from src.api.models import Cocktail, map_cocktail
+from src.database_commander import DB_COMMANDER as DBC
 
 router = APIRouter(tags=["cocktails"], prefix="/cocktails")
 
 
 @router.get("")
 async def get_cocktails(only_possible: bool = True):
-    return []
+    cocktails = DBC.get_possible_cocktails() if only_possible else DBC.get_all_cocktails()
+    return [map_cocktail(c) for c in cocktails]
 
 
 @router.get("/{cocktail_id}")
-async def get_cocktail(cocktail_id: int):
-    return {"cocktail_id": cocktail_id}
+async def get_cocktail(cocktail_id: int) -> Optional[Cocktail]:
+    cocktail = DBC.get_cocktail(cocktail_id)
+    return map_cocktail(cocktail)
 
 
-@router.post("/prepare/{cocktail_id}")
+@router.get("/prepare/{cocktail_id}")
 async def prepare_cocktail(cocktail_id: int, volume: int, alcohol_factor: float, is_virgin: bool = False):
     factor = alcohol_factor if not is_virgin else 0
-    return {"message": f"Cocktail {cocktail_id} of volume {volume} ({factor}%) prepared successfully!"}
+    cocktail = DBC.get_cocktail(cocktail_id)
+
+    # TODO: implement cocktail preparation with sockets
+    # return {"message": f"Cocktail {cocktail} of volume {volume} ({factor}%) prepared successfully!"}
+    async def event_generator():
+        i = 0
+        while i < 10:
+            i += 1
+            yield f"Cocktail {cocktail} of volume {volume} ({factor}%) {i}/10"
+            await asyncio.sleep(0.3)
+        yield "done"
+
+    return EventSourceResponse(event_generator(), ping=100)
 
 
 # TODO: Cocktail CRUD management
