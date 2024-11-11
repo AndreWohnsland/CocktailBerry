dea77f28c034d1ae30b46e213e8f9aabbb2f849b
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-11 19:23:22 +0100

    Adjust strong to weak private

--- dashboard/frontend/language.py
+++ dashboard/frontend/language.py
@@ -1,32 +1,31 @@
 import os
-from typing import Dict, List, Union
 from pathlib import Path
-from dotenv import load_dotenv
-import yaml
+from typing import Dict, List, Union
 
+import yaml
+from dotenv import load_dotenv
 
 DIRPATH = Path(__file__).parent.absolute()
 load_dotenv(DIRPATH / ".env")
 
 # Getting the language file as dict
 LANGUAGE_FILE = DIRPATH / "language.yaml"
-with open(LANGUAGE_FILE, "r", encoding="UTF-8") as stream:
+with open(LANGUAGE_FILE, encoding="UTF-8") as stream:
     LANGUAGE_DATA: Dict = yaml.safe_load(stream)
 
 
 class Language:
-
     def __init__(self):
-        self.HEADER = self.__choose_language(LANGUAGE_DATA["header_label"])
-        self.DEFAULT_MESSAGE = self.__choose_language(LANGUAGE_DATA["default_message"])
-        self.CALL_TO_ACTION = self.__choose_language(LANGUAGE_DATA["call_to_action"])
-        self.AMOUNT_TODAY = self.__choose_language(LANGUAGE_DATA["amount_today"])
-        self.VOLUME_TODAY = self.__choose_language(LANGUAGE_DATA["volume_today"])
-        self.AMOUNT_ALL = self.__choose_language(LANGUAGE_DATA["amount_all"])
-        self.VOLUME_ALL = self.__choose_language(LANGUAGE_DATA["volume_all"])
+        self.HEADER = self._choose_language(LANGUAGE_DATA["header_label"])
+        self.DEFAULT_MESSAGE = self._choose_language(LANGUAGE_DATA["default_message"])
+        self.CALL_TO_ACTION = self._choose_language(LANGUAGE_DATA["call_to_action"])
+        self.AMOUNT_TODAY = self._choose_language(LANGUAGE_DATA["amount_today"])
+        self.VOLUME_TODAY = self._choose_language(LANGUAGE_DATA["volume_today"])
+        self.AMOUNT_ALL = self._choose_language(LANGUAGE_DATA["amount_all"])
+        self.VOLUME_ALL = self._choose_language(LANGUAGE_DATA["volume_all"])
 
-    def __choose_language(self, element: dict, **kwargs) -> Union[str, List[str]]:
-        """Choose either the given language if exists, or english if not piping additional info into template"""
+    def _choose_language(self, element: dict, **kwargs) -> Union[str, List[str]]:
+        """Choose either the given language if exists, or english if not piping additional info into template."""
         language = os.getenv("UI_LANGUAGE")
         tmpl = element.get(language, element["en"])
         # Return the list and not apply template!
