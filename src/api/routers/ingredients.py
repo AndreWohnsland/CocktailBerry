e753d3909346353c3c67f479623c1c4093615aae
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-12 20:17:10 +0100

    Move db error to handler

--- src/api/routers/ingredients.py
+++ src/api/routers/ingredients.py
@@ -1,26 +1,28 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from src.api.models import IngredientInput, map_ingredient
-from src.database_commander import DB_COMMANDER as DBC
-from src.database_commander import DatabaseTransactionError
+from src.database_commander import DatabaseCommander
 
 router = APIRouter(tags=["ingredients"], prefix="/ingredients")
 
 
 @router.get("")
 async def get_ingredients(machine: bool = True, hand: bool = True):
+    DBC = DatabaseCommander()
     ingredients = DBC.get_all_ingredients(get_machine=machine, get_hand=hand)
     return [map_ingredient(i) for i in ingredients]
 
 
 @router.get("/{ingredient_id}")
 async def get_ingredient(ingredient_id: int):
+    DBC = DatabaseCommander()
     ingredient = DBC.get_ingredient(ingredient_id)
     return map_ingredient(ingredient)
 
 
 @router.post("")
 async def add_ingredient(ingredient: IngredientInput):
+    DBC = DatabaseCommander()
     DBC.insert_new_ingredient(
         ingredient_name=ingredient.name,
         alcohol_level=ingredient.alcohol,
@@ -36,6 +38,7 @@ async def add_ingredient(ingredient: IngredientInput):
 
 @router.put("/{ingredient_id}")
 async def update_ingredients(ingredient_id: int, ingredient: IngredientInput):
+    DBC = DatabaseCommander()
     DBC.set_ingredient_data(
         ingredient_name=ingredient.name,
         alcohol_level=ingredient.alcohol,
@@ -52,8 +55,6 @@ async def update_ingredients(ingredient_id: int, ingredient: IngredientInput):
 
 @router.delete("/{ingredient_id}")
 async def delete_ingredients(ingredient_id: int):
-    try:
-        DBC.delete_ingredient(ingredient_id)
-    except DatabaseTransactionError as e:
-        raise HTTPException(status_code=406, detail=str(e))
+    DBC = DatabaseCommander()
+    DBC.delete_ingredient(ingredient_id)
     return {"message": f"Ingredient {ingredient_id} was deleted!"}
