de988ae3ca655ec0232ca753982cadf193b42d19
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-17 13:57:24 +0100

    Add models

---
+++ web_client/src/types/models.ts
@@ -0,0 +1,78 @@
+export interface CocktailIngredient {
+  id: number;
+  name: string;
+  alcohol: number;
+  hand: boolean;
+  amount: number;
+  recipe_order: number;
+  unit: string;
+}
+
+export interface Cocktail {
+  id: number;
+  name: string;
+  alcohol: number;
+  amount: number;
+  enabled: boolean;
+  virgin_available: boolean;
+  ingredients: CocktailIngredient[];
+  image: string;
+}
+
+export interface Ingredient extends CocktailIngredient {
+  bottle_volume: number;
+  fill_level: number;
+  pump_speed: number;
+  cost: number;
+}
+
+export interface IngredientInput {
+  name: string;
+  alcohol: number;
+  bottle_volume: number;
+  fill_level: number;
+  cost: number;
+  pump_speed: number;
+  hand: boolean;
+  unit: string;
+}
+
+export interface CocktailIngredientInput {
+  id: number;
+  amount: number;
+  recipe_order: number;
+}
+
+export interface CocktailInput {
+  name: string;
+  enabled: boolean;
+  virgin_available: boolean;
+  ingredients: CocktailIngredientInput[];
+}
+
+export interface Bottle {
+  number: number;
+  ingredient?: Ingredient;
+}
+
+export type PrepareResult =
+  | 'VALIDATION_OK'
+  | 'IN_PROGRESS'
+  | 'FINISHED'
+  | 'CANCELED'
+  | 'NOT_ENOUGH_INGREDIENTS'
+  | 'COCKTAIL_NOT_FOUND'
+  | 'ADDON_ERROR'
+  | 'UNDEFINED';
+
+export interface CocktailStatus {
+  progress: number;
+  completed: boolean;
+  message?: string;
+  status: PrepareResult;
+}
+
+export interface ApiError {
+  detail: string;
+  status?: PrepareResult;
+}
