e59ec790650309da786324a2c632d48b4bb13cb7
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-19 21:18:40 +0100

    Apply custom style for button

--- web_client/src/components/bottle/BottleList.tsx
+++ web_client/src/components/bottle/BottleList.tsx
@@ -4,6 +4,7 @@ import { useIngredients } from '../../api/ingredients';
 import BottleComponent from './BottleComponent';
 import { FaGear } from 'react-icons/fa6';
 import { Ingredient } from '../../types/models';
+import { ToastContainer } from 'react-toastify';
 
 const BottleList: React.FC = () => {
   const { data: bottles, error: bottlesError, isLoading: bottlesLoading } = useBottles();
@@ -27,22 +28,28 @@ const BottleList: React.FC = () => {
   }
 
   return (
-    <div className='grid grid-cols-5 gap-2 p-4 w-full max-w-7xl' style={{ gridTemplateColumns: '2fr 3fr 6fr 4fr 1fr' }}>
-      {bottles?.map((bottle) => (
-        <BottleComponent
-          key={bottle.number}
-          bottle={bottle}
-          freeIngredients={freeIngredients}
-          setFreeIngredients={setFreeIngredients}
-        />
-      ))}
-      <button className='border-2 rounded-md border-primary text-primary col-span-2 p-2'>Apply</button>
-      <button className='border-2 rounded-md border-primary text-primary p-2'>Change</button>
-      <button className='border-2 rounded-md border-primary text-primary p-2'>Available</button>
-      <button className='border-2 rounded-md border-primary text-primary flex justify-center items-center max-w-12 p-2'>
-        <FaGear />
-      </button>
-    </div>
+    <>
+      <ToastContainer position='top-center' />
+      <div
+        className='grid grid-cols-5 gap-2 p-4 w-full max-w-7xl'
+        style={{ gridTemplateColumns: '2fr 3fr 6fr 4fr 1fr' }}
+      >
+        {bottles?.map((bottle) => (
+          <BottleComponent
+            key={bottle.number}
+            bottle={bottle}
+            freeIngredients={freeIngredients}
+            setFreeIngredients={setFreeIngredients}
+          />
+        ))}
+        <button className='button-primary-filled col-span-2 p-2'>Apply</button>
+        <button className='button-primary p-2'>Change</button>
+        <button className='button-primary-filled p-2'>Available</button>
+        <button className='button-primary-filled flex justify-center items-center max-w-12'>
+          <FaGear size={30} />
+        </button>
+      </div>
+    </>
   );
 };
 
