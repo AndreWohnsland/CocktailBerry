64988039d8d5966ce0d29fb37c8548d1ef17a898
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-22 18:43:06 +0100

    Ad route for available ingredients

--- src/api/routers/ingredients.py
+++ src/api/routers/ingredients.py
@@ -14,7 +14,7 @@ async def get_ingredients(machine: bool = True, hand: bool = True):
     return [map_ingredient(i) for i in ingredients]
 
 
-@router.get("/{ingredient_id}")
+@router.get("/{ingredient_id:int}")
 async def get_ingredient(ingredient_id: int):
     DBC = DatabaseCommander()
     ingredient = DBC.get_ingredient(ingredient_id)
@@ -37,7 +37,7 @@ async def add_ingredient(ingredient: IngredientInput):
     return map_ingredient(db_ingredient)
 
 
-@router.put("/{ingredient_id}")
+@router.put("/{ingredient_id:int}")
 async def update_ingredients(ingredient_id: int, ingredient: IngredientInput):
     DBC = DatabaseCommander()
     DBC.set_ingredient_data(
@@ -54,8 +54,14 @@ async def update_ingredients(ingredient_id: int, ingredient: IngredientInput):
     return {"message": f"Ingredient {ingredient_id} was updated to {ingredient}"}
 
 
-@router.delete("/{ingredient_id}")
+@router.delete("/{ingredient_id:int}")
 async def delete_ingredients(ingredient_id: int):
     DBC = DatabaseCommander()
     DBC.delete_ingredient(ingredient_id)
     return {"message": f"Ingredient {ingredient_id} was deleted!"}
+
+
+@router.get("/available")
+async def get_available_ingredients() -> list[int]:
+    DBC = DatabaseCommander()
+    return DBC.get_available_ids()
