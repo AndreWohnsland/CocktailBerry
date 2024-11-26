846fc0fdb42ccc7cd19ab8b3a9a87fedb8c2b2f6
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-26 19:50:24 +0100

    Add min height to root containers

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -6,23 +6,23 @@ import IngredientList from './components/ingredient/IngredientList.tsx';
 import RecipeList from './components/recipe/RecipeList.tsx';
 import BottleList from './components/bottle/BottleList.tsx';
 import AvailableBottles from './components/bottle/AvailableBottles.tsx';
-import AdjustBottles from './components/bottle/AdjustBottles.tsx';
+import OptionWindow from './components/options/OptionWindow.tsx';
 
 function App() {
   const { theme } = useTheme();
 
   return (
-    <div className={`${theme}`}>
+    <div className={`${theme} min-h-screen`}>
       <BrowserRouter>
         <Header />
-        <div className='app-container mt-12'>
+        <div className='app-container pt-12'>
           <Routes>
             <Route path='cocktails' element={<CocktailList />} />
             <Route path='ingredients' element={<IngredientList />} />
             <Route path='recipes' element={<RecipeList />} />
             <Route path='bottles' element={<BottleList />} />
             <Route path='bottles/available' element={<AvailableBottles />} />
-            <Route path='bottles/adjust' element={<AdjustBottles />} />
+            <Route path='options' element={<OptionWindow />} />
             <Route path='/' element={<Navigate to='/cocktails' />} />
           </Routes>
         </div>
