db432dcaf31233eb8942cb168d55b3ecd3f93366
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-11-28 20:01:48 +0100

    Center header items

--- web_client/src/components/Header.tsx
+++ web_client/src/components/Header.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useTheme } from '../ThemeProvider.tsx';
 
 const Header: React.FC = () => {
-  const { onThemeChange } = useTheme();
-  const themes = ['default', 'berry', 'bavaria', 'alien', 'custom'];
-
   const getNavLinkClass = (isActive: boolean) => {
     const baseClass = 'nav-link px-2 flex items-center border-2 font-semibold';
     const activeClass = 'text-background bg-secondary border-secondary rounded-full';
@@ -14,8 +10,8 @@ const Header: React.FC = () => {
 
   return (
     <header className='fixed top-0 left-0 right-0 bg-background shadow-sm shadow-neutral z-10'>
-      <div className='flex justify-between items-center py-1 px-4'>
-        <nav className='flex space-x-1'>
+      <div className='flex justify-center items-center py-1 px-4'>
+        <nav className='flex space-x-1 overflow-x-auto'>
           <NavLink to='/cocktails' className={({ isActive }) => getNavLinkClass(isActive)}>
             Cocktails
           </NavLink>
@@ -28,16 +24,10 @@ const Header: React.FC = () => {
           <NavLink to='/bottles' className={({ isActive }) => getNavLinkClass(isActive)}>
             Bottles
           </NavLink>
+          <NavLink to='/options' className={({ isActive }) => getNavLinkClass(isActive)}>
+            Options
+          </NavLink>
         </nav>
-        <div className='dropdown-container'>
-          <select className='theme-dropdown select-base w-full px-2' onChange={(e) => onThemeChange(e.target.value)}>
-            {themes.map((theme) => (
-              <option key={theme} value={theme}>
-                {theme.charAt(0).toUpperCase() + theme.slice(1)}
-              </option>
-            ))}
-          </select>
-        </div>
       </div>
     </header>
   );
