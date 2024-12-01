8bb0c1505e2c3d03d45f86de50ef711c3cdbc6c1
Andre Wohnsland <andre.wohnsland@tado.com>
2024-12-01 15:16:16 +0100

    Add proper cleaning flow

--- web_client/src/components/options/OptionWindow.tsx
+++ web_client/src/components/options/OptionWindow.tsx
@@ -13,55 +13,56 @@ import {
 import { ToastContainer } from 'react-toastify';
 import { confirmAndExecute, executeAndShow } from '../../utils';
 import { useNavigate } from 'react-router-dom';
+import ProgressModal from '../cocktail/ProgressModal';
+import { useState } from 'react';
 
 const OptionWindow = () => {
-  const { onThemeChange } = useTheme();
+  const { theme, onThemeChange } = useTheme();
   const navigate = useNavigate();
+  const [isProgressModalOpen, setIsProgressModalOpen] = useState(false);
   const themes = ['default', 'berry', 'bavaria', 'alien', 'custom'];
 
-  const cleanMachineButtonClick = async () => {
-    try {
-      const result = await cleanMachine();
-      console.log(result);
-    } catch (error) {
-      console.error('Error executing cleanMachine action:', error);
-    }
-  };
-
-  const configurationButtonClick = async () => {
-    try {
-      const result = await Promise.resolve(console.log('Configuration action'));
-      console.log(result);
-    } catch (error) {
-      console.error('Error executing configuration action:', error);
-    }
+  const cleanClick = async () => {
+    const started = await executeAndShow(cleanMachine);
+    if (!started) return;
+    setIsProgressModalOpen(true);
   };
 
   return (
     <>
       <ToastContainer position='top-center' />
+      <ProgressModal
+        isOpen={isProgressModalOpen}
+        onRequestClose={() => setIsProgressModalOpen(false)}
+        progress={0}
+        displayName={'Cleaning the Machine'}
+      />
       <div className='flex flex-col items-center max-w-5xl w-full p-2 pt-0'>
         <div className='dropdown-container flex flex-row items-center mb-4'>
           <p className='mr-2'>Theme:</p>
-          <select className='theme-dropdown select-base w-full p-2' onChange={(e) => onThemeChange(e.target.value)}>
-            {themes.map((theme) => (
-              <option key={theme} value={theme}>
-                {theme.charAt(0).toUpperCase() + theme.slice(1)}
+          <select
+            className='theme-dropdown select-base w-full p-2'
+            value={theme}
+            onChange={(e) => onThemeChange(e.target.value)}
+          >
+            {themes.map((t) => (
+              <option key={t} value={t}>
+                {t.charAt(0).toUpperCase() + t.slice(1)}
               </option>
             ))}
           </select>
         </div>
         <div className='grid gap-1 w-full grid-cols-1 md:grid-cols-2'>
-          <button className='button-primary p-4' onClick={cleanMachineButtonClick}>
+          <button className='button-primary-filled p-4' onClick={cleanClick}>
             Cleaning
           </button>
           <button className='button-primary p-4' onClick={() => navigate('/calibration')}>
             Calibration
           </button>
-          <button className='button-primary p-4' onClick={configurationButtonClick}>
+          <button className='button-primary-filled p-4' onClick={() => navigate('/options/configuration')}>
             Configuration
           </button>
-          <button className='button-primary p-4' onClick={async () => Promise.resolve(console.log('Data action'))}>
+          <button className='button-primary p-4' onClick={() => navigate('/data')}>
             Data
           </button>
           <button className='button-primary p-4' onClick={async () => Promise.resolve(console.log('Backup action'))}>
@@ -76,13 +77,16 @@ const OptionWindow = () => {
           <button className='button-primary p-4' onClick={() => confirmAndExecute('shutdown', shutdownSystem)}>
             Shutdown
           </button>
-          <button className='button-primary p-4' onClick={async () => Promise.resolve(console.log('Log action'))}>
+          <button className='button-primary p-4' onClick={() => navigate('/logs')}>
             Logs
           </button>
-          <button className='button-primary p-4' onClick={updateSystem}>
+          <button className='button-primary-filled p-4' onClick={updateSystem}>
             Update System
           </button>
-          <button className='button-primary p-4 col-span-1 md:col-span-2' onClick={updateSoftware}>
+          <button
+            className='button-primary-filled p-4 col-span-1 md:col-span-2'
+            onClick={() => executeAndShow(updateSoftware)}
+          >
             Update CocktailBerry Software
           </button>
           <button className='button-primary p-4' onClick={updateWifiData}>
