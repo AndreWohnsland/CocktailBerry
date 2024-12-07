10d1f97058f240052fb260404e7dbe94246349fa
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-07 09:08:44 +0100

    Add viewport height properties

--- web_client/tailwind.config.js
+++ web_client/tailwind.config.js
@@ -11,6 +11,12 @@ export default {
         danger: 'var(--danger-color)',
         text: 'var(--text-color)',
       },
+      screens: {
+        'h-xs': { raw: '(min-height: 400px)' },
+        'h-sm': { raw: '(min-height: 640px)' },
+        'h-md': { raw: '(min-height: 768px)' },
+        'h-lg': { raw: '(min-height: 1024px)' },
+      },
     },
   },
   plugins: [],
