26d536824e94eb1570c5b05ac7caa71aa5737af9
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-07 09:11:18 +0100

    Make tab selector stay on top

--- web_client/src/components/options/TabSelector.tsx
+++ web_client/src/components/options/TabSelector.tsx
@@ -7,14 +7,14 @@ interface TabSelectorProps {
 }
 
 const createClass = (isActive: boolean) => {
-  const baseClass = 'px-2 flex items-center border-2 font-semibold';
+  const baseClass = 'px-1 flex items-center border-2 font-semibold';
   const activeClass = 'text-background bg-secondary border-secondary rounded-full';
   return isActive ? `${baseClass} ${activeClass}` : `${baseClass} border-transparent`;
 };
 
 const TabSelector: React.FC<TabSelectorProps> = ({ selectedTab, onSelectTab }) => {
   return (
-    <div className='flex flex-row sticky top-9 py-2 bg-background z-9 w-full items-center justify-center overflow-auto'>
+    <div className='flex flex-row fixed top-9 py-2 bg-background z-9 w-full items-center justify-center overflow-auto'>
       {OPTIONTABS.map((tab) => (
         <button key={tab} onClick={() => onSelectTab(tab)} className={createClass(selectedTab === tab)}>
           {tab}
