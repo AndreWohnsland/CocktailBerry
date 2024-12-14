b59384fcaad4d14ec0af46f455914db180a29e8f
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-14 16:11:29 +0100

    Adjust nginx file

--- src/migration/setup_nginx.py
+++ src/migration/setup_nginx.py
@@ -1,49 +1,25 @@
 import subprocess
 from pathlib import Path
 
+from src.filepath import NGINX_CONFIG_FILE
 
-def setup_nginx(
-    config_path="/etc/nginx/sites-available/cocktailberry_web_client",
-    web_root="/var/www/cocktailberry_web_client",
-    server_name="localhost",
-):
-    """Install and configures Nginx to serve a React app.
 
-    :param config_path: Path to the Nginx configuration file.
-    :param web_root: Directory where the React app's build files are located.
-    :param server_name: The server name (e.g., localhost or domain).
-    """
+def setup_nginx():
+    """Install and configures Nginx to serve a React app."""
+    config_path = Path("/etc/nginx/sites-available/cocktailberry_web_client")
+    config_path_enabled = Path("/etc/nginx/sites-enabled/cocktailberry_web_client")
+    web_root = Path("/var/www/cocktailberry_web_client")
     try:
         # Install Nginx
         subprocess.run(["sudo", "apt", "update"], check=True)
         subprocess.run(["sudo", "apt", "install", "-y", "nginx"], check=True)
 
         # Create the web root directory if it doesn't exist
-        Path(web_root).mkdir(parents=True, exist_ok=True)
-
-        # Create Nginx configuration
-        nginx_config = f"""
-        server {{
-            listen 80;
-            server_name {server_name};
-
-            root {web_root};
-            index index.html;
-
-            location / {{
-                try_files $uri /index.html;
-            }}
-        }}
-        """
-
-        # Write the configuration to the file
-        config_path = Path(config_path)
-        config_path.write_text(nginx_config)
+        web_root.mkdir(parents=True, exist_ok=True)
+        config_path.write_text(NGINX_CONFIG_FILE.read_text())
 
         # Enable the site
-        subprocess.run(
-            ["sudo", "ln", "-s", str(config_path), "/etc/nginx/sites-enabled/cocktailberry_web_client"], check=True
-        )
+        subprocess.run(["sudo", "ln", "-s", str(config_path), str(config_path_enabled)], check=True)
 
         # Remove default Nginx site if exists
         default_path = Path("/etc/nginx/sites-enabled/default")
