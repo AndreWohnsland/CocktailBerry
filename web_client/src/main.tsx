60b6591805c924584f83c235534172a9404cea11
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-15 09:21:53 +0100

    Provide configuration context within app
    Used to alter app based on user set config

--- web_client/src/main.tsx
+++ web_client/src/main.tsx
@@ -5,15 +5,18 @@ import { ThemeProvider } from './ThemeProvider.tsx';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { ConfigProvider } from './ConfigProvider.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <ConfigProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ConfigProvider>
       {import.meta.env.VITE_APP_DEV === 'true' && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
