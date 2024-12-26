6b1411e9dddc1012b328606ae4bb2e367bd6bf7e
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-26 09:34:19 +0100

    Also store auth string in state

--- web_client/src/AuthProvider.tsx
+++ web_client/src/AuthProvider.tsx
@@ -4,47 +4,57 @@ import { useConfig } from './ConfigProvider';
 
 interface AuthContextType {
   masterAuthenticated: boolean;
-  makerAuthenticated: boolean;
   setMasterAuthenticated: (authenticated: boolean) => void;
+  makerAuthenticated: boolean;
   setMakerAuthenticated: (authenticated: boolean) => void;
+  masterPassword: number;
+  setMasterPassword: (password: number) => void;
+  makerPassword: number;
+  setMakerPassword: (password: number) => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [masterAuthenticated, setMasterAuthenticated] = useState(false);
+  const [masterPassword, setMasterPassword] = useState(0);
   const [makerAuthenticated, setMakerAuthenticated] = useState(false);
+  const [makerPassword, setMakerPassword] = useState(0);
   const location = useLocation();
   const { config } = useConfig();
   // need to check which tabs are locked and as soon we leave those tabs set the authenticated state to false
   let protectedManagementPaths: string[] = [];
   if (config.UI_LOCKED_TABS) {
     protectedManagementPaths = [
-      (config.UI_LOCKED_TABS as boolean[])[0] ? '/manage/ingredients' : '',
-      (config.UI_LOCKED_TABS as boolean[])[1] ? '/manage/recipes' : '',
-      (config.UI_LOCKED_TABS as boolean[])[2] ? '/manage/bottles' : '',
+      config.UI_LOCKED_TABS[0] ? '/manage/ingredients' : '',
+      config.UI_LOCKED_TABS[1] ? '/manage/recipes' : '',
+      config.UI_LOCKED_TABS[2] ? '/manage/bottles' : '',
     ].filter((path) => path !== '');
   }
 
   useEffect(() => {
     if (!location.pathname.startsWith('/options') && masterAuthenticated) {
       setMasterAuthenticated(false);
+      setMasterPassword(0);
     }
     if (!protectedManagementPaths.some((path) => location.pathname.startsWith(path)) && makerAuthenticated) {
       setMakerAuthenticated(false);
+      setMakerPassword(0);
     }
-  }, [
-    location,
-    masterAuthenticated,
-    makerAuthenticated,
-    setMasterAuthenticated,
-    setMakerAuthenticated,
-    protectedManagementPaths,
-  ]);
+  }, [location, masterAuthenticated, makerAuthenticated, protectedManagementPaths]);
 
   return (
     <AuthContext.Provider
-      value={{ masterAuthenticated, makerAuthenticated, setMasterAuthenticated, setMakerAuthenticated }}
+      value={{
+        masterAuthenticated,
+        setMasterAuthenticated,
+        makerAuthenticated,
+        setMakerAuthenticated,
+        masterPassword,
+        setMasterPassword,
+        makerPassword,
+        setMakerPassword,
+      }}
     >
       {children}
     </AuthContext.Provider>
