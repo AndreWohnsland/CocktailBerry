c615f154eea1a03e21c57a92e67b4ef3e74e020f
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-12-26 09:35:16 +0100

    Do not insert theme in container
    We already do this over the context manager into the root

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -1,27 +1,32 @@
-import CocktailList from './components/cocktail/CocktailList.tsx';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Header from './components/Header.tsx';
-import { useConfig } from './ConfigProvider.tsx';
-import IngredientList from './components/ingredient/IngredientList.tsx';
-import RecipeList from './components/recipe/RecipeList.tsx';
-import BottleList from './components/bottle/BottleList.tsx';
+import Modal from 'react-modal';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import AvailableBottles from './components/bottle/AvailableBottles.tsx';
-import OptionWindow from './components/options/OptionWindow.tsx';
+import BottleList from './components/bottle/BottleList.tsx';
+import CocktailList from './components/cocktail/CocktailList.tsx';
+import GettingConfiguration from './components/common/GettingConfiguration.tsx';
+import { MakerPasswordProtected, MasterPasswordProtected } from './components/common/ProtectedRoute.tsx';
+import Header from './components/Header.tsx';
+import IngredientList from './components/ingredient/IngredientList.tsx';
+import AddonManager from './components/options/AddonManager.tsx';
 import CalibrationWindow from './components/options/CalibrationWindow.tsx';
 import ConfigWindow from './components/options/ConfigWindow.tsx';
 import DataWindow from './components/options/DataWindow.tsx';
 import LogWindow from './components/options/LogWindow.tsx';
+import OptionWindow from './components/options/OptionWindow.tsx';
 import WifiManager from './components/options/WifiManager.tsx';
-import { ToastContainer } from 'react-toastify';
-import GettingConfiguration from './components/common/GettingConfiguration.tsx';
-import AddonManager from './components/options/AddonManager.tsx';
-import { MakerPasswordProtected, MasterPasswordProtected } from './components/common/ProtectedRoute.tsx';
+import RecipeList from './components/recipe/RecipeList.tsx';
+import { useConfig } from './ConfigProvider.tsx';
+import useAxiosInterceptors from './hooks/useAxiosInterceptors.ts';
+
+Modal.setAppElement('#root');
 
 function App() {
-  const { theme, config } = useConfig();
+  const { config } = useConfig();
+  useAxiosInterceptors();
 
   return (
-    <div className={`${theme} min-h-screen`}>
+    <div className='min-h-screen'>
       <Header />
       <div className='app-container pt-12'>
         <ToastContainer position='top-center' />
