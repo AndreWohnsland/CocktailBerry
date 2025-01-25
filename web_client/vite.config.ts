24a78e4cc3145b81a6b5b65de92ed46dae1b27b6
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-01-25 16:01:38 +0100

    Try to use custom domain in build

--- web_client/vite.config.ts
+++ web_client/vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  if (mode === 'demo') {
-    return {
-      plugins: [react()],
-      base: '/CocktailBerry/',
-    };
-  }
-  return { plugins: [react()] };
+export default defineConfig({
+  plugins: [react()],
 });
+// export default defineConfig(({ mode }) => {
+//   if (mode === 'demo') {
+//     return {
+//       plugins: [react()],
+//       base: '/CocktailBerry/',
+//     };
+//   }
+//   return { plugins: [react()] };
+// });
