50f4540c9debd016a15baee736ee0f07e95bbecd
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-06-02 08:05:06 +0200

    do not pass sys arguments into qt app
    This would only be used if over CLI the user wants to change some qt specific attributes.

--- src/programs/cocktailberry.py
+++ src/programs/cocktailberry.py
@@ -5,12 +5,15 @@ from PyQt5.QtWidgets import QApplication
 
 from src.error_handler import logerror
 from src.ui.setup_mainwindow import MainScreen
+from src.utils import time_print
 
 
 @logerror
 def run_cocktailberry() -> None:
     """Execute the cocktail program."""
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # type: ignore
-    app = QApplication(sys.argv)
+    app = QApplication([])
     MainScreen()
-    sys.exit(app.exec_())
+    qt_code = app.exec()
+    time_print(f"CocktailBerry exited with code {qt_code}.")
+    sys.exit(qt_code)
