229bb6a6f71c8c57961c8552e2b4c220aecf79b1
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-09-22 18:48:03 +0200

    add action card component

--- web_client/src/components/IssuePage.tsx
+++ web_client/src/components/IssuePage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { ignoreIssues, useIssues } from '../api/options';
-import LoadingData from './common/LoadingData';
-import ErrorComponent from './common/ErrorComponent';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import { FaRegClock } from 'react-icons/fa';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
-import { executeAndShow, hasStartupIssues } from '../utils';
 import { FaGear } from 'react-icons/fa6';
+import { useNavigate } from 'react-router';
+import { ignoreIssues, useIssues } from '../api/options';
+import { executeAndShow, hasStartupIssues } from '../utils';
+import ActionCard from './common/ActionCard';
+import ErrorComponent from './common/ErrorComponent';
+import LoadingData from './common/LoadingData';
 
 const IssuePage: React.FC = () => {
   const { data: issues, error, isLoading, refetch } = useIssues();
@@ -40,42 +41,36 @@ const IssuePage: React.FC = () => {
     <div className='w-full h-full flex flex-col items-center justify-center max-w-lg p-2'>
       <h1 className='text-secondary text-2xl font-bold mb-4'>{t('issues.systemIssues')}</h1>
       {hasIssues && (
-        <div className='w-full mt-4 border rounded-lg border-neutral items-center justify-center flex flex-col p-2'>
-          <p className='text-center text-neutral'>{t('issues.info')}</p>
-          <button onClick={applyResetIssue} className='button-danger p-2 mt-2 w-full items-center justify-center flex'>
-            <AiOutlineCloseCircle className='mr-3' size={22} />
-            {t('issues.reset')}
-          </button>
-        </div>
+        <ActionCard
+          header={t('issues.infoHeader')}
+          sections={[t('issues.info')]}
+          actionText={t('issues.reset')}
+          onActionClick={applyResetIssue}
+          actionIcon={AiFillCloseCircle}
+          actionStyle='danger'
+        />
       )}
 
       {issues?.internet.has_issue && (
-        <div className='w-full mt-4 border rounded-lg border-neutral items-center justify-center flex flex-col p-2'>
-          <h2 className='text-secondary text-center text-xl mb-2'>{t('issues.InternetConnectionIssue')}</h2>
-          <p className='text-center'>{t('issues.noInternetConnection')}</p>
-          <button onClick={goToTime} className='button-primary-filled p-2 mt-2 w-full items-center justify-center flex'>
-            <FaRegClock className='mr-3' size={22} /> {t('issues.adjustTime')}
-          </button>
-        </div>
+        <ActionCard
+          header={t('issues.InternetConnectionIssue')}
+          sections={[t('issues.noInternetConnection')]}
+          actionText={t('issues.adjustTime')}
+          onActionClick={goToTime}
+          actionIcon={FaRegClock}
+        />
       )}
       {issues?.deprecated.has_issue && (
-        <div className='w-full mt-4 border rounded-lg border-neutral items-center justify-center flex flex-col p-2'>
-          <h2 className='text-secondary text-center text-xl mb-2'>{t('issues.deprecationWaring')}</h2>
-          <p className='text-center'>{t('issues.systemPythonDeprecated')}</p>
-        </div>
+        <ActionCard header={t('issues.deprecationWaring')} sections={[t('issues.systemPythonDeprecated')]} />
       )}
       {issues?.config.has_issue && (
-        <div className='w-full mt-4 border rounded-lg border-neutral items-center justify-center flex flex-col p-2'>
-          <h2 className='text-secondary text-center text-xl mb-2'>{t('issues.configIssue')}</h2>
-          <p className='text-center'>{t('issues.configSetToDefault')}</p>
-          <p className='text-center'>{issues.config.message}</p>
-          <button
-            onClick={goToConfig}
-            className='button-primary-filled p-2 mt-2 w-full items-center justify-center flex'
-          >
-            <FaGear className='mr-3' size={22} /> {t('issues.goToConfig')}
-          </button>
-        </div>
+        <ActionCard
+          header={t('issues.configIssue')}
+          sections={[t('issues.configSetToDefault'), issues.config.message]}
+          actionText={t('issues.goToConfig')}
+          onActionClick={goToConfig}
+          actionIcon={FaGear}
+        />
       )}
       {!hasIssues && (
         <div className='items-center justify-center  flex flex-col'>
