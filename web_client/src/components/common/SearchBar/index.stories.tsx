e5366c13c8de4ab4fe81e203198c10628453581a
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-09-25 19:05:57 +0200

    adjust searchbar

--- web_client/src/components/common/SearchBar/index.stories.tsx
+++ web_client/src/components/common/SearchBar/index.stories.tsx
@@ -4,10 +4,16 @@ import { useState } from 'react';
 import SearchBar from '.';
 
 const meta = {
-  title: 'Example/SearchBar',
+  title: 'Common/SearchBar',
   component: SearchBar,
   parameters: {
     layout: 'centered',
+  },
+  argTypes: {
+    search: { control: 'text' },
+    setSearch: { action: 'setSearch' },
+    afterInput: { control: 'object' },
+    initiallyOpen: { control: 'boolean' },
   },
   tags: ['autodocs'],
 } satisfies Meta<typeof SearchBar>;
@@ -16,12 +22,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const DefaultInitialClosed: Story = {
-  args: { search: '', setSearch: () => {}, afterInput: undefined },
+  args: { search: '', setSearch: () => {}, afterInput: undefined, initiallyOpen: false },
   render: (args) => {
-    const [search, setSearch] = useState<string | null>('');
     return (
       <div className='w-[420px]'>
-        <SearchBar search={search} setSearch={setSearch} initiallyOpen={args.initiallyOpen} />
+        <SearchBar search={args.search} setSearch={args.setSearch} initiallyOpen={args.initiallyOpen} />
       </div>
     );
   },
