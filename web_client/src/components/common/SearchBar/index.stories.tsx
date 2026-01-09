7c392a7be283fd1593dbf48c40ffacc7410b0a45
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2026-01-09 11:11:45 +0100

    fix story missing new flag

--- web_client/src/components/common/SearchBar/index.stories.tsx
+++ web_client/src/components/common/SearchBar/index.stories.tsx
@@ -22,48 +22,49 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const DefaultInitialClosed: Story = {
-  args: { search: '', setSearch: () => {}, afterInput: undefined, initiallyOpen: false },
+  args: { search: '', tabBarVisible: true, setSearch: () => {}, afterInput: undefined, initiallyOpen: false },
   render: (args) => {
     return (
       <div className='w-[420px]'>
-        <SearchBar search={args.search} setSearch={args.setSearch} initiallyOpen={args.initiallyOpen} />
+        <SearchBar search={args.search} tabBarVisible setSearch={args.setSearch} initiallyOpen={args.initiallyOpen} />
       </div>
     );
   },
 };
 
 export const InitiallyOpen: Story = {
-  args: { search: '', setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
+  args: { search: '', tabBarVisible: true, setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
   render: (args) => {
     const [search, setSearch] = useState<string | null>('');
     return (
       <div className='w-[420px]'>
-        <SearchBar search={search} setSearch={setSearch} initiallyOpen={args.initiallyOpen} />
+        <SearchBar search={search} tabBarVisible setSearch={setSearch} initiallyOpen={args.initiallyOpen} />
       </div>
     );
   },
 };
 
 export const WithInitialSearch: Story = {
-  args: { search: 'mojito', setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
+  args: { search: 'mojito', tabBarVisible: true, setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
   render: (args) => {
     const [search, setSearch] = useState<string | null>('mojito');
     return (
       <div className='w-[420px]'>
-        <SearchBar search={search} setSearch={setSearch} initiallyOpen={args.initiallyOpen} />
+        <SearchBar search={search} tabBarVisible setSearch={setSearch} initiallyOpen={args.initiallyOpen} />
       </div>
     );
   },
 };
 
 export const WithAfterInput: Story = {
-  args: { search: '', setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
+  args: { search: '', tabBarVisible: true, setSearch: () => {}, afterInput: undefined, initiallyOpen: true },
   render: (args) => {
     const [search, setSearch] = useState<string | null>('');
     return (
       <div className='w-[520px]'>
         <SearchBar
           search={search}
+          tabBarVisible
           setSearch={setSearch}
           afterInput={<button className='button-secondary px-2 h-10'>Filter</button>}
           initiallyOpen={args.initiallyOpen}
