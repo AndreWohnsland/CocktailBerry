a4b6bdcd15d78742e66ca59d8926d5aa78335b8f
KrisMorr <154343071+KrisMorr@users.noreply.github.com>
2026-05-05 14:57:36 +0200

    Add polish translation
    Add files via upload
    
    Co-authored-by: Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>

--- web_client/src/i18n/index.ts
+++ web_client/src/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import translationDE from '../locales/de/translation.json';
 import translationEN from '../locales/en/translation.json';
+import translationPL from '../locales/pl/translation.json';
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -11,6 +12,9 @@ i18n.use(initReactI18next).init({
     de: {
       translation: translationDE,
     },
+    pl: {
+      translation: translationPL,
+      }
   },
   lng: 'en',
   fallbackLng: 'en',
